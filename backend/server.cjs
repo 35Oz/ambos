8f29a81ee29088fe3d51d22416a3053c45cb3bfb
Eze Diaz <ezediaz_00@hotmail.com>
2025-06-15 22:16:34 -0300

    se arreglo el bloqueo de cors

--- backend/server.cjs
+++ backend/server.cjs
@@ -8,11 +8,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ambos-1.onrender.com'
+];
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
   methods: ['GET', 'POST'],
   credentials: true
 }));
